8d8150ab588c45140a1c5af97dbba73ef937c94f
Fernando Muñoz <dam209f@gmail.com>
2022-11-18 19:54:41 +0100

    Clase Posicion 2.

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Posicion.java
@@ -3,4 +3,54 @@ package org.iesalandalus.programacion.reinaajedrez;
 public class Posicion {
     private int fila;
     private char columna;
+    
+    public void setFila(int fila){
+        if(fila<=0 | fila>=9){
+            throw new IllegalArgumentException("Fila incorrecta.");
+        }
+        else{
+            this.fila = fila;
+        }
+    }
+    public void setColumna(String columna){
+        switch (columna) {
+            case "a":
+                this.columna=columna.charAt(0);
+                break;
+            case "b":
+                this.columna=columna.charAt(0);
+                break;
+            case "c":
+                this.columna=columna.charAt(0);
+                break;
+            case "d":
+                this.columna=columna.charAt(0);
+                break;
+            case "e":
+                this.columna=columna.charAt(0);
+                break;
+            case "f":
+                this.columna=columna.charAt(0);
+                break;
+            case "g":
+                this.columna=columna.charAt(0);
+                break;
+            case "h":
+                this.columna=columna.charAt(0);
+                break;
+            default:
+            throw new IllegalArgumentException("Fila incorrecta.");
+        }
+    }
+    public int getFila(){
+        return this.fila;
+    }
+    public char getColumna(){
+        return this.columna;
+    }
+    
+
+
+
+
 }
