722b4f543cd0f40fc3e6fe18a253bd38b2193535
Fmuntor <dam209f@gmail.com>
2022-11-28 20:21:11 +0100

    3. Clase Consola

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
@@ -4,5 +4,7 @@ public class Consola {
 
     private Consola() {
     }
-    
+    public void mostrarMenu(){
+        System.out.println("Seleccione una opcion:\na) Crear reina por defecto.\nb)Crear reina eligiendo el color.\nc)mover.\nd)Salir.");
+    }
 }
