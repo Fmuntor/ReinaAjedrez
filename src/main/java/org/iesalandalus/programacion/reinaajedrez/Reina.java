fc0d307ff8e2954b4083f22aa42db8ce70944a88
Fmuntor <dam209f@gmail.com>
2022-11-28 19:18:31 +0100

    2. Clase Reina

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Reina.java
@@ -3,5 +3,18 @@ package org.iesalandalus.programacion.reinaajedrez;
 public class Reina {
     Color color;
     Posicion posicion;
+    public Color getColor() {
+        return color;
+    }
+    private void setColor(Color color) {
+        this.color = color;
+    }
+    public Posicion getPosicion() {
+        return posicion;
+    }
+    private void setPosicion(Posicion posicion) {
+        this.posicion = posicion;
+    }
+
 }
 
